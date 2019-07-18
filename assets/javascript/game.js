c4607d74344d6d0da38f74a90396b884a19adbfd
Shelly <35303907+slane1980@users.noreply.github.com>
2019-07-17 20:19:13 -0700

    clean up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,109 @@
-// Creates an array that lists out all of the options for letters
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
-// List of all Variables
-var lettersGuessedText = document.getElementById("lettersGuessedText");
-var computerChoiceText = document.getElementById("computerchoice-text");
-var userWin = document.getElementById("userWins");
-var userLoss = document.getElementById("userLosses");
+//variables to keep track of wins, losses, guesses left and letters already guessed
 var wins = 0;
-var lostround = 0;
-var guessesAvailable = 10;
-var lettersGuessed = [];
+var losses = 0;
+var guessesLeft = 5;
+var alreadyGuessed = [];
+var gameMessage = "Guess What Letter I'm thinking of by pressing a key";
+var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-// Randomly chooses a choice from the options array. This is the Computer's guess.
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-// This function is run whenever the user presses a key.
-document.onkeyup = function (event) {
-
-    //Display Wins and Losses
-    userLosses.textContent = "Losses: " + lostround;
-    userWins.textContent = "Wins: " + wins;
-
-
-     // If user runs out of guesses end code and reset game.
-    if (guessesAvailable === 0) {
-        alert("Game Over");
-        lostround++;
-        resetGame();
-        return;
-    }
-
-    // Determines which key was pressed and converts to lower case.
-    var userGuess = event.key.toLowerCase();
-
-
-    // Only run the following code block if the userGuess equals an alphabet letter.
-
-    if (computerChoices.indexOf(userGuess) > -1 && lettersGuessed.indexOf(userGuess) === -1) {
-       
-        // Adds the userGuess to the array lettersGuessed.
-        lettersGuessed.push(userGuess);
-
-        // Display all user guess.
-        lettersGuessedText.textContent = "Your Guesses So Far: " + lettersGuessed;
-
-        //Display Guesses Available
-        guessesLeft.textContent = "Guesses Left: " + guessesAvailable;
-
-        
-        //If userguess is equal to computer guess    
-        if (userGuess === computerGuess) {
-            wins++;
-            resetGame();
-            console.log(userWin);
-            alert("Way to go! You must be Psychic!");
-        }
-
-        // If userguess is not equal to computer guess
-        if (userGuess !== computerGuess) {
-            guessesAvailable--;
-        }
-
-
-
-    }
+function updateGameMessage() {
+  document.getElementById("gameMessage").textContent = gameMessage;
 }
 
+//global function to display our game info on the screen
+function displayStats() {
+  document.getElementById("guessesLeft").textContent =
+    "Guesses Left: " + guessesLeft;
+  document.getElementById("userGuesses").textContent =
+    "Already Guessed Letters: " + alreadyGuessed;
+  document.getElementById("wins").textContent = "Wins: " + wins;
+  document.getElementById("losses").textContent = "Losses: " + losses;
+}
+
+//global function to reset the game
 function resetGame() {
-    guessesAvailable = 10;
-    lettersGuessed = [];
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+  alreadyGuessed = [];
+  guessesLeft = 5;
+  displayStats();
 }
+
+//invoke display stats function to display all our info on page load
+displayStats();
+updateGameMessage();
+
+//on key up event handler
+document.onkeyup = function(event) {
+  //take the key the user presses and sets it equal to our userGuess variable
+  var userGuess = event.key.toLowerCase();
+  gameMessage = "You Guessed " + userGuess;
+  updateGameMessage();
+
+  //logging the computer guess and the user guess to the console
+  console.log("Computer Guess: " + computerGuess);
+  console.log("User Guess: " + userGuess);
+  //decrese guesses Left by one
+  guessesLeft--;
+  //update game info on screen
+  displayStats();
+  // Check to see if there are guesses available
+
+  //check if the  userGues is a letter  and also if that letter has already been guessed
+  if (
+    alphabet.indexOf(userGuess) > -1 &&
+    alreadyGuessed.indexOf(userGuess) === -1
+  ) {
+    //if the key is a letter and it hasn't already been guessed we want to store it in our alreadyGuessed array and decrease our guesses left
+    alreadyGuessed.push(userGuess);
+    //update screen
+    displayStats();
+
+    //check if userGuess is equal to the computerguess
+    if (userGuess === computerGuess) {
+      //add to the wins
+      wins++;
+      gameMessage = "You Won! Guess a Letter to Play Again.";
+      updateGameMessage();
+      //reset the game
+      resetGame();
+    }
+    if (guessesLeft === 0) {
+      //increase the losses
+      losses++;
+      gameMessage =
+        "Sorry! You ran out of guesses.  Guess a Letter to Play Again.";
+      updateGameMessage();
+      //reset the game
+      resetGame();
+    }
+  }
+};
